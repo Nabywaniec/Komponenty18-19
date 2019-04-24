029b5457f4ae3ee86d9e728569071324deef16dd
Tomasz <kurek.tom@gmail.com>
2019-04-24 18:11:47 +0200

    Imlement getDispatchListLength, getVertexNum functions and mTSP constructor

--- src/main/java/Jmetal/DispatchListsProblem.java
+++ src/main/java/Jmetal/DispatchListsProblem.java
@@ -3,7 +3,6 @@ package Jmetal;
 import org.uma.jmetal.problem.Problem;
 
 public interface DispatchListsProblem<S extends DispatchListsSolution> extends Problem<S> {
-    int getDispatchListsNum();
     int getDispatchListLength();
     int getVertexNum();
 }
--- src/main/java/Jmetal/mTSP.java
+++ src/main/java/Jmetal/mTSP.java
@@ -1,21 +1,28 @@
 package Jmetal;
 
+import Model.Graph;
 import org.uma.jmetal.problem.impl.AbstractGenericProblem;
 
 public class mTSP extends AbstractGenericProblem<DispatchListsSolution> implements DispatchListsProblem<DispatchListsSolution>{
-    @Override
-    public int getDispatchListsNum() {
-        return 0;
+    private int dispatchListLength;
+    private Graph graph;
+
+    public mTSP(Graph graph, int dispatchListLength){
+        this.graph = graph;
+        this.dispatchListLength = dispatchListLength;
+        this.setNumberOfVariables(this.graph.getVertexNum()*this.dispatchListLength);
+        this.setNumberOfObjectives(1);
+        this.setName("mTSP");
     }
 
     @Override
     public int getDispatchListLength() {
-        return 0;
+        return this.dispatchListLength;
     }
 
     @Override
     public int getVertexNum() {
-        return 0;
+        return this.graph.getVertexNum();
     }
 
     @Override
