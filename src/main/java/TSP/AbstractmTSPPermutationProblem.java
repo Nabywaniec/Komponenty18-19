b5aeb8d0a3ab15c310fb34f456957159813af4b1
Tomasz <kurek.tom@gmail.com>
2019-05-25 15:44:37 +0200

    Add AbstractmTSPPermutationProblem

---
+++ src/main/java/TSP/AbstractmTSPPermutationProblem.java
@@ -0,0 +1,11 @@
+package TSP;
+
+import org.uma.jmetal.problem.impl.AbstractGenericProblem;
+
+public abstract class AbstractmTSPPermutationProblem
+        extends AbstractGenericProblem<mTSPPermutationSolution<Integer>>
+        implements mTSPPermutationProblem<mTSPPermutationSolution<Integer>> {
+    public AbstractmTSPPermutationProblem(){
+
+    }
+}
